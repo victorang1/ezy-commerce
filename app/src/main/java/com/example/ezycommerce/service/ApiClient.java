3f3113c4573b906b11b0b99251e801dccb8b65cc
Victor <angvictor91@gmail.com>
2021-01-30 09:02:25 +0700

    adding cleint and service

---
+++ app/src/main/java/com/example/ezycommerce/service/ApiClient.java
@@ -0,0 +1,23 @@
+package com.example.ezycommerce.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static Retrofit mClient;
+
+    private Retrofit getClient() {
+        if (mClient == null) {
+            mClient = new Retrofit.Builder()
+                    .baseUrl("https://u73olh7vwg.execute-api.ap-northeast-2.amazonaws.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return mClient;
+    }
+
+    public ApiService service() {
+        return getClient().create(ApiService.class);
+    }
+}
---
+++ app/src/main/java/com/example/ezycommerce/service/ApiService.java
@@ -0,0 +1,5 @@
+package com.example.ezycommerce.service;
+
+public interface ApiService {
+
+}
